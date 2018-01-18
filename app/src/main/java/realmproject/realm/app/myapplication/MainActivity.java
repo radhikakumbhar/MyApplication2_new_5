26f6ea8e11adeaf6f1dcedef2c4e454790c3504c
tanajipochapure <pochapure@gmail.com>
2018-01-18 23:40:46 +0530

    changes commit

--- app/src/main/java/realmproject/realm/app/myapplication/MainActivity.java
+++ app/src/main/java/realmproject/realm/app/myapplication/MainActivity.java
@@ -1,15 +1,36 @@
 package realmproject.realm.app.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+import realmproject.realm.app.myapplication.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         //tanaji commit from github account
         //tanaji commit from github account
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User();
+        user.setName("Android");
+
+        binding.setUser(user);
+        binding.setActivity(this);
+    }
+
+    public void onButtonClick(String email) {
+
+        Log.d("", "Email :" + binding.getUser().getEmail());
+        Log.d("", "Email : " + email);
+        Toast.makeText(this, email, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/realmproject/realm/app/myapplication/User.java
@@ -0,0 +1,27 @@
+package realmproject.realm.app.myapplication;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+public class User extends BaseObservable {
+
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Bindable
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        notifyPropertyChanged(BR.email);
+    }
+}
